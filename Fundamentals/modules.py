74bdea24c6c45d5c6d7b7932bb7632f31e391dab
jod8bd <jod8bd@mail.missouri.edu>
2020-10-12 01:41:08 -0500

    Uploading Fundamental Python Code

---
+++ Fundamentals/modules.py
@@ -0,0 +1,8 @@
+# import the math module, which contains features for working with math
+import math
+
+# the math module contains lots of pre-built functions
+print("The square root of 16 is", math.sqrt(16))
+
+# in addition to functions, some modules contain useful constants
+print("Pi is:", math.pi)
